eee430e56a4be1a484dc0d187c2565b444376551
NetEgo <dawilk@o2.pl>
2012-09-16 00:09:54 +0200

    C1

---
+++ Test1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Dawid
+ * Date: 16.09.12
+ * Time: 00:09
+ * To change this template use File | Settings | File Templates.
+ */
+class Test1
+{
+
+}
